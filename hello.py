556be955986346da75d0c7f372fb50cb72b823f6
cel01_ <102827591+celocodes@users.noreply.github.com>
2024-03-14 20:30:50 -0300

    asasf

--- hello.py
+++ hello.py
@@ -58,6 +58,6 @@ def index():
                            remote_host=session.get('host'),
                            current_time=datetime.utcnow())
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
 def login():
     return render_template('login.html', name=session.get('name'), current_time=datetime.utcnow())
