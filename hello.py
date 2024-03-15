af36cb17d883190d60961bf0ffb2ae753cf90bc5
cel01_ <102827591+celocodes@users.noreply.github.com>
2024-03-14 22:04:29 -0300

    asdasd

--- hello.py
+++ hello.py
@@ -65,7 +65,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if request.method == 'POST':
+    if form.validate_on_submit():
         session['email'] = form.email.data
         return redirect(url_for('loginResponse'))
     return render_template('login.html', form=form, current_time=datetime.utcnow())
